c0dce5cf7402e05d5a4ef0bf7ea149554603afc6
Alex House <house.alexander@gmail.com>
2021-04-05 13:19:19 +0100

    Add image to main screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require('./assets/adaptive-icon.png')} width="auto" height="300" style={{height: 300}} />
     </View>
   );
 }
